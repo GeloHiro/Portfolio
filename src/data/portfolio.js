3eea003e0920d214bd74f46eabbab8d29fcf005f
GeloHiro <angelo.corvera@gmail.com>
2023-05-01 17:23:07 -0400

    Initial commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "TBA",
+    imgUrl: "/assets/javascript-g7c92ece53_1280.png",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://google.com",
+  },
+  {
+    title: "TBA",
+    imgUrl: "/assets/javascript.png",
+    stack: ["React", "Tailwind", "Vite"],
+    link: "https://google.com",
+  },
+  {
+    title: "TBA",
+    imgUrl: "/assets/javascript2.png",
+    stack: ["Python", "C++", "Robotics"],
+    link: "https://google.com",
+  },
+];
---
+++ src/data/timeline.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    year: "Today",
+    title: "Lab Associate at Yale",
+    company: "Yale",
+    duration: "6+ months",
+    details: "I take care of patient's specimens",
+  },
+  {
+    year: "Today",
+    title: "UPS Store Associate",
+    duration: "2 years",
+    details: "I help customers deal with their packages",
+  },
+  {
+    year: "Today",
+    title: "Gateway Community College Student",
+    duration: "2021 - Today",
+    details: "I am studying for Computer Engineering, AS",
+  },
+];
